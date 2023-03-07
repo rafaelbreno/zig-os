f52fee8597220802fb77cb009c70290565500a27
Rafael Breno <rafael_breno@outlook.com>
2023-03-07 14:33:53 -0300

    feat: Add Key modifiers

--- src/terminal/keyboard.zig
+++ src/terminal/keyboard.zig
@@ -4,3 +4,24 @@ pub const State = enum {
     Released,
     Hit,
 };
+
+pub const Modifiers = struct {
+    rightShift: bool = false,
+    leftShift: bool = false,
+    rightAlt: bool = false,
+    leftAlt: bool = false,
+    rightCtrl: bool = false,
+    leftCtrl: bool = false,
+
+    pub fn isShiftPressed(self: *const Modifiers) bool {
+        return self.rightShift or self.leftShift;
+    }
+
+    pub fn isAltPressed(self: *const Modifiers) bool {
+        return self.rightAlt or self.leftAlt;
+    }
+
+    pub fn isCtrlPressed(self: *const Modifiers) bool {
+        return self.rightCtrl or self.leftCtrl;
+    }
+};
