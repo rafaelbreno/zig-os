6369f4011e3e7695e19f624cd609574f02d17d02
Rafael Breno <32229014+rafaelbreno@users.noreply.github.com>
2026-05-27 15:58:33 -0300

    Add callconv(.naked) to _start function signature

--- kernel/main.zig
+++ kernel/main.zig
@@ -1,5 +1,8 @@
 // _start is the entry point of the OS
-//`noreturn` is used because a infinite loop doesn't return anything.
-export fn _start() noreturn {
+// `noreturn` is used because a infinite loop doesn't return anything.
+// callconv(.naked)
+//  The callconv specifier changes the calling convention of the function.
+//  .naked makes it no have a prologue/epilogue
+export fn _start() callconv(.naked) noreturn {
     while (true) {}
 }
